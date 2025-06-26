c40e5a12d1bbabacb9d35e24983a4d2010bd8764
anushkasuryavanshi <anushkasuryavanshi15@gmail.com>
2025-06-26 15:49:12 +0530

    changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.data.datasensorgraph"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
